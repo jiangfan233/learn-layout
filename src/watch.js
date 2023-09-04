e68f36a210ec50d74c5fbc92c7dd433ee5fad424
jiang <jiang@W11-20230221719.lan>
2023-09-04 22:11:33 +0800

    update

---
+++ src/watch.js
@@ -0,0 +1,65 @@
+const { effect } = require("../src/effect");
+
+function watch(source, cb, options = {}) {
+  let getter;
+  if (typeof source === "function") {
+    getter = source;
+  } else {
+    getter = () => traverse(source);
+  }
+
+  let oldValue;
+  let newValue;
+
+  // 当副作用函数为一个异步函数时，返回的结果不一定是根据最新的状态得到的
+  // 这是需要使用 onInvalidate 控制
+  let cleanUp;
+  function onInvalidate(fn) {
+    cleanUp = fn;
+  }
+
+  const job = () => {
+    // 在scheduler中拿到新值
+    // 这里运用了js的变量提升
+    newValue = effectFn();
+    if (cleanUp) cleanUp();
+    cb(newValue, oldValue, onInvalidate);
+    oldValue = newValue;
+  };
+
+  const effectFn = effect(() => getter(), {
+    lazy: true,
+    scheduler() {
+      // 表示在组件更新后执行副作用函数
+      // 也就是在UI更新后执行副作用函数
+      if (options.flush === "post") {
+        Promise.resolve().then(job);
+      } else {
+        // options.flush === "sync"
+        job();
+
+        // options.flush === "pre": 表示在组件更新前执行副作用函数
+      }
+    },
+  });
+
+  if (options.immediate) {
+    job();
+  } else {
+    // 直接执行一次effectFn拿到旧值
+    oldValue = effectFn();
+  }
+}
+
+function traverse(value, seen = new Set()) {
+  if (typeof value !== "object" || value === null || seen.has(value)) return;
+  seen.add(value);
+  for (let k in value) {
+    traverse(value[k], seen);
+  }
+  return value;
+}
+
+module.exports = {
+  watch,
+};
---
+++ tests/watch.test.js
@@ -0,0 +1,109 @@
+const { watch } = require("../src/watch");
+const { proxyObject } = require("../src/effect");
+
+test("watch test", () => {
+  const data = { foo: 1 };
+  const obj = proxyObject(data);
+
+  const fn = jest.fn((curr, prev) => {
+    obj.foo;
+    expect(curr).toBe(obj.foo);
+    expect(curr).toBe(2);
+    expect(prev).toBe(curr > 1 ? curr - 1 : undefined);
+  });
+  watch(() => obj.foo, fn);
+
+  expect(fn).toHaveBeenCalledTimes(0);
+
+  obj.foo++;
+
+  expect(fn).toHaveBeenCalledTimes(1);
+});
+
+test("test watch immediate option", () => {
+  const data = { foo: 1 };
+  const obj = proxyObject(data);
+
+  const fn = jest.fn((curr, prev) => {
+    obj.foo;
+    expect(curr).toBe(obj.foo);
+    expect(prev).toBe(curr > 1 ? curr - 1 : undefined);
+  });
+  watch(() => obj.foo, fn, { immediate: true });
+
+  expect(fn).toHaveBeenCalledTimes(1);
+
+  obj.foo++;
+
+  expect(fn).toHaveBeenCalledTimes(2);
+});
+
+test("test watch flush option", () => {
+  const data = { foo: 1 };
+  const obj = proxyObject(data);
+
+  const fn = jest.fn((curr, prev) => {
+    obj.foo;
+    expect(curr).toBe(obj.foo);
+    expect(prev).toBe(curr > 1 ? curr - 1 : undefined);
+  });
+  watch(() => obj.foo, fn, { flush: "post" });
+
+  expect(fn).toHaveBeenCalledTimes(0);
+
+  obj.foo++;
+
+  expect(fn).toHaveBeenCalledTimes(0);
+
+  setTimeout(() => {
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+});
+
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
+
+test("test watch onInvalidate argument", () => {
+  const data = { foo: 1 };
+  const obj = proxyObject(data);
+
+  let n = 3;
+  let id;
+  let res;
+  const once = jest.fn(() => {
+    res = n;
+  });
+
+  const func = () => {
+    id = setTimeout(once, n-- * 1000);
+  };
+
+  const clear = jest.fn(() => {
+    expect(id).toBeTruthy();
+    clearTimeout(id);
+  });
+
+  const fn = jest.fn((curr, prev, onInvalidate) => {
+    func();
+    onInvalidate(clear);
+  });
+
+  watch(() => obj.foo, fn);
+
+  expect(fn).toHaveBeenCalledTimes(0);
+
+  obj.foo++;
+
+  // Fast-forward until all timers have been executed
+  jest.advanceTimersByTime(4000);
+
+  expect(fn).toHaveBeenCalledTimes(1);
+  expect(clear).toHaveBeenCalledTimes(0);
+
+  obj.foo++;
+
+  expect(fn).toHaveBeenCalledTimes(2);
+  expect(clear).toHaveBeenCalledTimes(1);
+  expect(res).toBe(2);
+  expect(once).toHaveBeenCalledTimes(1);
+});
